1ac20fef6c11b37eac327b83b0d699367bcf449d
ShanMehr <ishanmeher25@gmail.com>
2025-05-19 23:29:19 -0700

    paper database

---
+++ platform_schema.sql
@@ -0,0 +1,19 @@
+-- Drop Tables if they exist
+DROP TABLE if EXISTS paper_vector_db;
+
+--  Drop the database if it exists
+DROP DATABASE IF EXISTS research_platform;
+
+-- Create and switch to the database
+CREATE DATABASE research_platform;
+\c research_platform;
+
+-- Create Tables
+CREATE TABLE paper_vector_db (
+    id SERIAL PRIMARY KEY,
+    paper_id VARCHAR(255) NOT NULL,
+    embedding VECTOR(768) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    abstract TEXT
+);
+
